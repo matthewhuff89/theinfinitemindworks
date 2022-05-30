3482abecb157f4f8dcc5b2d407d989329c1b56dd
Matthew D. Huff <matthewhuff89@gmail.com>
2022-05-30 20:16:36 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>The Infinite Mindworks</h1>
+      <p>mystery & soulfulness</p>
+    </main>
+  );
+}
+
+export default App;
