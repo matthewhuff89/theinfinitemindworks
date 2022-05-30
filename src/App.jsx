18e0c0c202f6029d5ceb65f4410a70bed95cd71a
Matthew D. Huff <matthewhuff89@gmail.com>
2022-05-30 20:27:07 +0000

    added temp logo and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import logo from './images/ss_mindworks_b.jpg';
 
 function App() {
   return (
     <main>
       <h1>The Infinite Mindworks</h1>
+      <img src={logo} />
       <p>mystery & soulfulness</p>
+      <a href='https://twitter.com/s0ulDirect0r/status/1500924056157229058'>Anansi Stories</a>
+      <br></br>
+      <a href='https://twitter.com/s0ulDirect0r/status/1523323511543975937'>The Spidernomicon</a>
     </main>
   );
 }
