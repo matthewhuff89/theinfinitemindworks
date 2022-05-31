3708dc16c12f3b6f49b3936bd2faf3a389e10beb
Matthew D. Huff <matthewhuff89@gmail.com>
2022-05-31 02:25:08 +0000

    added some more links

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import logo from './images/ss_mindworks_b.jpg';
 function App() {
   return (
     <main>
-      <h1>The Infinite Mindworks</h1>
       <img src={logo} />
       <p>mystery & soulfulness</p>
       <a href='https://twitter.com/s0ulDirect0r/status/1500924056157229058'>Anansi Stories</a>
-      <br></br>
+      <br />
       <a href='https://twitter.com/s0ulDirect0r/status/1523323511543975937'>The Spidernomicon</a>
+      <br />
+      <a href='https://twitter.com/s0ulDirect0r/status/1467931820385705990?s=20&t=KmdhYC3jvMzXopzzprG3MQ'>Poesis - The 100 Poem Thread</a>
+      <br />
+      <a href='https://twitter.com/s0ulDirect0r/status/1528308440140873728?s=20&t=KmdhYC3jvMzXopzzprG3MQ'>The Wastelands - A Short Graphic Tale</a>
+      <br />
+      <a href='https://twitter.com/s0ulDirect0r/status/1528178093143166976?s=20&t=KmdhYC3jvMzXopzzprG3MQ'>The Prometheus Project</a>
     </main>
   );
 }
