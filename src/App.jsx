5ed5996976261b88bf7a995731b9d786b9ab0e32
Matthew D. Huff <matthewhuff89@gmail.com>
2022-05-31 02:46:54 +0000

    added a link to my NFTs

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
       <p>mystery & soulfulness</p>
       <a href='https://twitter.com/s0ulDirect0r/status/1500924056157229058'>Anansi Stories</a>
       <br />
+      <a href='https://zora.co/souldirector.eth?status=LISTED&collection='>NFT Artifacts (on Zora)</a>
+      <br />
       <a href='https://twitter.com/s0ulDirect0r/status/1523323511543975937'>The Spidernomicon</a>
       <br />
       <a href='https://twitter.com/s0ulDirect0r/status/1467931820385705990?s=20&t=KmdhYC3jvMzXopzzprG3MQ'>Poesis - The 100 Poem Thread</a>
